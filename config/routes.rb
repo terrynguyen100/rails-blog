f41c7fdfe8e0c86a27b4effd09fff8d90d7533d2
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-27 12:32:03 -0600

    add view /articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  get "/articles", to: "articles#index"
+  #The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
